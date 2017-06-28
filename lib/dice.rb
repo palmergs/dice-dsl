a6540fc7ba59238167086e3294186daa98f13c12
Galen Palmer <palmergs@gmail.com>
2017-06-27 21:21:47 -0400

    add requires

--- lib/dice.rb
+++ lib/dice.rb
@@ -3,3 +3,9 @@ end
 
 require 'dice/token'
 require 'dice/tokenizer'
+
+require 'dice/simple_roll'
+require 'dice/modified_roll'
+require 'dice/modify_each_roll'
+require 'dice/exploding_roll'
+require 'dice/explode_each_roll'
