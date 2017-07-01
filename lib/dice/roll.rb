f5a3741caf9ed260496e7908a99149fdf1170c3d
Galen Palmer <palmergs@gmail.com>
2017-07-01 13:23:38 -0400

    remove modifier from base roll

--- lib/dice/roll.rb
+++ lib/dice/roll.rb
@@ -1,30 +1,11 @@
 module Dice
-  Roll = Struct.new(:value, :range, :modifier) do
-    def modifier?
-      return false unless modifier
-      modifier != 0
-    end
-
-    def bonus?
-      modifier? && modifier > 0
-    end
-
-    def penalty?
-      modifier? && modifier < 0
-    end
-
-    def modifier_to_s
-      return '' unless modifier?
-      return "+#{ modifier }" if bonus?
-      return "#{ modifier }" if penalty?
-    end
-
+  Roll = Struct.new(:value, :range) do
     def to_s
-      "#{ value } (1d#{ range }#{ modifier_to_s })"
+      "#{ value } (1d#{ range })"
     end
 
     def to_a
-      [ value, range, modifier ? 0 : modifier ]
+      [ value, range ]
     end
   end
 end
--- spec/dice/roll_spec.rb
+++ spec/dice/roll_spec.rb
@@ -3,19 +3,14 @@ require 'dice'
 
 RSpec.describe Dice::Roll do
   describe '.to_s' do
-    it 'renders without modifier' do
+    it 'renders to string' do
       r = Dice::Roll.new(4, 6)
       expect(r.to_s).to eq("4 (1d6)")
     end
 
-    it 'renders with positive modifier' do
-      r = Dice::Roll.new(4, 6, 2)
-      expect(r.to_s).to eq("4 (1d6+2)")
-    end
-
-    it 'renders with negative modifier' do
-      r = Dice::Roll.new(7, 20, -2)
-      expect(r.to_s).to eq("7 (1d20-2)")
+    it 'can be converted to an array' do
+      r = Dice::Roll.new(3, 4)
+      expect(r.to_a).to eq([3, 4])
     end
   end
 end
